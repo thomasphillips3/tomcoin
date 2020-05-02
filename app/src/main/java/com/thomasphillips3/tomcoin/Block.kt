4a3fac7f6829fa1ecc8dec88ef5129134c56ab5b
Thomas Phillips <thomasphillips3@gmail.com>
2020-05-02 14:24:46 -0700

    implement mining

--- app/src/main/java/com/thomasphillips3/tomcoin/Block.kt
+++ app/src/main/java/com/thomasphillips3/tomcoin/Block.kt
@@ -6,17 +6,18 @@ data class Block(
     val previousHash: String,
     val data: String,
     val timestamp: Long = Instant.now().toEpochMilli(),
+    val nonce: Long = 0,
     var hash: String = ""
 ) {
     init { hash = calculateHash() }
 
-    private fun calculateHash(): String {
-        return "$previousHash$data$timestamp".hash()
+    fun calculateHash(): String {
+        return "$previousHash$data$timestamp$nonce".hash()
     }
 
     companion object {
         fun create(previousHash: String, data: String): Block {
-            return Block(previousHash, data, Instant.now().toEpochMilli(),"")
+            return Block(previousHash, data, Instant.now().toEpochMilli(), 0, "")
         }
     }
 }
